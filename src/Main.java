625ac94ef24e2add40faa24c4ed86d7e0147fc39
Tuzaeva Anna <tuzaeva.anna@mail.ru>
2020-02-24 09:25:48 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,158 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    static final int SIZE = 3;
+    static final int DOTS_TO_WIN = 3;
+
+    static final char DOT_X = 'X';
+    static final char DOT_O = 'O';
+    static final char DOT_EMPTY = '.';
+
+    static char[][] map;
+
+    static Random random = new Random();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+
+        while (true) {
+            humanTurn();
+            printMap();
+
+            if (checkWin(DOT_X)) {
+                System.out.println("Игрок ПОБЕДИЛ!!!");
+                break;
+            }
+
+            if (isFull()) {
+                System.out.println("Ничья!");
+                break;
+            }
+
+            aiTurn();
+            turnHumanWinCell();
+            printMap();
+
+            if (checkWin(DOT_O)) {
+                System.out.println("Искуственный интеллект ПОБЕДИЛ!!!");
+                break;
+            }
+
+            if (isFull()) {
+                System.out.println("Ничья!");
+                break;
+            }
+        }
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        System.out.print("  ");
+        for (int i = 1; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате Y X (ряд столбец)");
+            y = sc.nextInt() - 1;
+            x = sc.nextInt() - 1;
+        } while (!isCellValid(y, x));
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int y, int x) {
+        if (x < 0 || y < 0 || x >= SIZE || y >= SIZE) {
+            return false;
+        }
+        return map[y][x] == DOT_EMPTY;
+    }
+
+    public static void aiTurn() {
+        int x, y;
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (isCellValid(i, j)) {
+                    map[i][j] = DOT_X;
+                    if (checkWin(DOT_X)) {
+                        map[i][j] = DOT_O;
+                        return;
+                    }
+                    map[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+
+        do {
+            y = random.nextInt(SIZE);
+            x = random.nextInt(SIZE);
+        } while (!isCellValid(y, x));
+        map[y][x] = DOT_O;
+    }
+
+    public static boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean turnHumanWinCell() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (isCellValid(i, j)) {
+                    map[i][j] = DOT_X;
+                    if (checkWin(DOT_X)) {
+                        map[i][j] = DOT_O;
+                        return true;
+                    }
+                    map[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean checkWin(char symbol) {
+        if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol){ return true; }
+        if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol){ return true; }
+        if (map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol){ return true; }
+
+        if (map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol){ return true; }
+        if (map[0][1] == symbol && map[1][1] == symbol && map[2][1] == symbol){ return true; }
+        if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol){ return true; }
+
+        if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol){ return true; }
+        if (map[0][2] == symbol && map[1][1] == symbol && map[2][0] == symbol){ return true; }
+
+        return false;
+    }
 }
+
+
